6e0a7da367dba2c55ce0668b0cad47442f041af4
Jeremie <lucheng989898@gmail.com>
2019-11-11 14:09:27 +0800

    update export

--- ts/index.ts
+++ ts/index.ts
@@ -1,9 +1,9 @@
-export {TusServer as Server} from './lib/Server';
+export {TusServer} from './lib/Server';
 export {DataStore} from './lib/stores/DataStore';
 export {FileStore} from './lib/stores/FileStore';
 export {GCSDataStore} from './lib/stores/GCSDataStore';
 export {S3Store} from './lib/stores/S3Store';
 export {MongoGridFSStore} from './lib/stores/MongoGridFSStore';
 
-export {ERRORS as ERRORS, EVENTS as EVENTS} from './lib/constants';
+export {ERRORS, EVENTS} from './lib/constants';
 
