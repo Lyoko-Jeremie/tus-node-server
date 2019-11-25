a368451b00500e449ffba021b791673c3bd002cc
Jeremie <lucheng989898@gmail.com>
2019-11-25 17:34:39 +0800

    getMetaData expose more api

--- ts/index.ts
+++ ts/index.ts
@@ -4,7 +4,7 @@ export {FileStore} from './lib/stores/FileStore';
 export {GCSDataStore} from './lib/stores/GCSDataStore';
 export {S3Store} from './lib/stores/S3Store';
 export {MongoGridFSStore} from './lib/stores/MongoGridFSStore';
-export {getMetaDataNotDecode, getMetaData} from './lib/tools/getMetaData';
+export {getMetaDataNotDecode, getMetaData, decodeMetaData, splitMetaData} from './lib/tools/getMetaData';
 
 export {ERRORS, EVENTS, EXPOSED_HEADERS, EXPOSED_HEADERS_LIST} from './lib/constants';
 
