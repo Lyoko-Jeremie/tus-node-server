d709f0fae78b852b75ec2b98a711f9e4b1aeeb47
Jeremie <lucheng989898@gmail.com>
2020-03-10 15:07:54 +0800

    export all export

--- ts/index.ts
+++ ts/index.ts
@@ -1,10 +1,10 @@
-export {TusServer} from './lib/Server';
-export {DataStore} from './lib/stores/DataStore';
-export {FileStore} from './lib/stores/FileStore';
-export {GCSDataStore} from './lib/stores/GCSDataStore';
-export {S3Store} from './lib/stores/S3Store';
-export {MongoGridFSStore} from './lib/stores/MongoGridFSStore';
-export {getMetaDataNotDecode, getMetaData, decodeMetaData, splitMetaData} from './lib/tools/getMetaData';
+export * from './lib/Server';
+export * from './lib/stores/DataStore';
+export * from './lib/stores/FileStore';
+export * from './lib/stores/GCSDataStore';
+export * from './lib/stores/S3Store';
+export * from './lib/stores/MongoGridFSStore';
+export * from './lib/tools/getMetaData';
 
-export {ERRORS, EVENTS, EXPOSED_HEADERS, EXPOSED_HEADERS_LIST} from './lib/constants';
+export * from './lib/constants';
 
